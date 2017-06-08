7f43f398f9dd392e580a17784dc80866f5c2885e
tsobieroy <tsobieroy@linux>
2017-06-08 13:01:02 +0200

    Adding executeCalibration function

--- src/aqualight/visualisation/CalibrationController.java
+++ src/aqualight/visualisation/CalibrationController.java
@@ -5,7 +5,12 @@
  */
 package aqualight.visualisation;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
@@ -16,12 +21,41 @@ import javafx.fxml.Initializable;
  */
 public class CalibrationController implements Initializable {
 
+    
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
     
+    /**
+     * @param pathToProgram addresses the path to the program e.g. "/aqualight-phcontroller-calibrator"
+     * @brief execute c program from java (not tested yet)
+     * @param Address i2c address of the probe
+     * @param Value which ph / ec  or whatever is addressed
+     * @return 
+     */
+    public boolean executeCalibration(String pathToProgram, String Address, String Value) {
+        try {
+            Process process = new ProcessBuilder(pathToProgram, Address, Value).start();
+            InputStream is = process.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String line;
+            long[] args = null;
+
+            System.out.printf("Output of running %s is:", Arrays.toString(args));
+
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException ex) {
+            return false;
+        }
+        return false;
+    }
+
 }
